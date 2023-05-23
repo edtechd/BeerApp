66bb3f8a312447e86b24cc5f4cda4566c1704282
Ed <support@edtech-corp.com>
2023-05-24 02:00:55 +0300

    Added descriptions for Breweries types

---
+++ BeerApp - Senior/src/types/typeDescriptions.ts
@@ -0,0 +1,16 @@
+import { TYPE } from './types';
+
+const TYPE_DESCRIPTIONS: Record<TYPE, string> = {
+  micro: 'Microbrewery',
+  nano: 'Nano Brewery',
+  regional: 'Regional Brewery',
+  brewpub: 'Brewpub',
+  large: 'Large Brewery',
+  planning: 'Planning Brewery',
+  bar: 'Bar',
+  contract: 'Contract Brewery',
+  proprietor: 'Proprietor Brewery',
+  closed: 'Closed Brewery',
+};
+
+export default TYPE_DESCRIPTIONS;
